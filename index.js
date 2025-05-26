a775126bb29be3c1a90ab770767740f0d7028b7f
minecraftchandan <87936472+minecraftchandan@users.noreply.github.com>
2025-05-26 09:52:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const container = document.getElementById("container");
-    const goRegister = document.getElementById("goRegister");
-    const goLogin = document.getElementById("goLogin");
+const goRegister = document.getElementById("goRegister");
+const goLogin = document.getElementById("goLogin");
 
-    const signInForm = document.getElementById("signInForm");
-    const signUpForm = document.getElementById("signUpForm");
-    const placeholder = document.getElementById("formPlaceholder");
+goRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.classList.add("active");
+});
 
-    goRegister.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (window.innerWidth <= 760) {
-        signInForm.classList.remove("active");
-        signUpForm.classList.add("active");
-        placeholder.classList.add("active");
-      } else {
-        container.classList.add("active");
-        placeholder.classList.remove("active");
-      }
-    });
-
-    goLogin.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (window.innerWidth <= 760) {
-        signUpForm.classList.remove("active");
-        signInForm.classList.add("active");
-        placeholder.classList.add("active");
-      } else {
-        container.classList.remove("active");
-        placeholder.classList.remove("active");
-      }
-    })
+goLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.classList.remove("active");
+});
